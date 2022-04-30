d590b87594763eb18605aa37ed17ad9f51d42964
Sri-1227 <srinivasmekala1227@gmail.com>
2022-04-30 22:19:28 +0530

    Srinivas Created a Digital Clock

---
+++ #3 Javascript Clock/Srinivas/clock.js
@@ -0,0 +1,25 @@
+const hour = document.querySelector(".hour-number");
+const minute = document.querySelector(".minute-number");
+const second = document.querySelector(".second-number");
+
+function displaytime() {
+  const time = new Date();
+  let hours = time.getHours();
+  let minutes = time.getMinutes();
+  let seconds = time.getSeconds();
+
+  if (hours < 10) {
+    hour.innerHTML = "0" + hours + " :";
+  } else {
+    hour.innerHTML = hours + " :";
+  }
+  if (minutes < 10) {
+    minute.innerHTML = "0" + minutes + " :";
+  } else {
+    minute.innerHTML = minutes + " :";
+  }
+  second.innerHTML = seconds;
+  requestAnimationFrame(displaytime);
+}
+
+displaytime();
